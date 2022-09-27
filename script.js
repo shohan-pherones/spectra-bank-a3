8c22bb707adddf46a1cd862c08bff935dc2c2d6b
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-27 23:40:45 +0600

    currency and set timeout added

--- script.js
+++ script.js
@@ -10,40 +10,40 @@ const accounts = [
     movements: [2500, 500, -750, 1200, 3200, -1500, 500, 1200, -1750, 1800],
     interestRate: 1.5, // %
     password: 1234,
-    // movementsDates: [
-    //   "2021-11-18T21:31:17.178Z",
-    //   "2021-12-23T07:42:02.383Z",
-    //   "2022-01-28T09:15:04.904Z",
-    //   "2022-04-01T10:17:24.185Z",
-    //   "2022-07-08T14:11:59.604Z",
-    //   "2022-09-10T17:01:17.194Z",
-    //   "2022-09-12T23:36:17.929Z",
-    //   "2022-09-15T12:51:31.398Z",
-    //   "2022-09-19T06:41:26.190Z",
-    //   "2022-09-21T08:11:36.678Z",
-    // ],
-    // currency: "USD",
-    // locale: "en-US",
+    movementsDates: [
+      "2021-11-18T21:31:17.178Z",
+      "2021-12-23T07:42:02.383Z",
+      "2022-01-28T09:15:04.904Z",
+      "2022-04-01T10:17:24.185Z",
+      "2022-07-08T14:11:59.604Z",
+      "2022-09-10T17:01:17.194Z",
+      "2022-09-12T23:36:17.929Z",
+      "2022-09-15T12:51:31.398Z",
+      "2022-09-19T06:41:26.190Z",
+      "2022-09-21T08:11:36.678Z",
+    ],
+    currency: "USD",
+    locale: "en-US",
   },
   {
     owner: "Sunerah Binte Ayesha",
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -300, 1500, -1850],
     interestRate: 1.3, // %
     password: 5678,
-    // movementsDates: [
-    //   "2021-12-11T21:31:17.671Z",
-    //   "2021-12-27T07:42:02.184Z",
-    //   "2022-01-05T09:15:04.805Z",
-    //   "2022-02-14T10:17:24.687Z",
-    //   "2022-03-12T14:11:59.203Z",
-    //   "2022-05-16T17:01:17.392Z",
-    //   "2022-08-10T23:36:17.522Z",
-    //   "2022-09-03T12:51:31.491Z",
-    //   "2022-09-18T06:41:26.394Z",
-    //   "2022-09-21T08:11:36.276Z",
-    // ],
-    // currency: "EUR",
-    // locale: "en-GB",
+    movementsDates: [
+      "2021-12-11T21:31:17.671Z",
+      "2021-12-27T07:42:02.184Z",
+      "2022-01-05T09:15:04.805Z",
+      "2022-02-14T10:17:24.687Z",
+      "2022-03-12T14:11:59.203Z",
+      "2022-05-16T17:01:17.392Z",
+      "2022-08-10T23:36:17.522Z",
+      "2022-09-03T12:51:31.491Z",
+      "2022-09-18T06:41:26.394Z",
+      "2022-09-21T08:11:36.276Z",
+    ],
+    currency: "EUR",
+    locale: "en-GB",
   },
 ];
 
@@ -85,6 +85,16 @@ function updateUI(currentAccount) {
   displaySummary(currentAccount);
   displayBalance(currentAccount);
 }
+/////////////////////////////////////////
+// formatting currency
+/////////////////////////////////////////
+
+function formatCurrency(value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+}
 
 /////////////////////////////////////////////////////////////////////
 // Username
@@ -115,18 +125,22 @@ btnLogin.addEventListener("click", function (e) {
   );
 
   if (currentAccount?.password === Number(inputLoginPassword.value)) {
-    // Display UI and welcome
-    labelWelcome.textContent = `Welcome back, ${currentAccount.owner
-      .split(" ")
-      .at(0)}`;
-    containerApp.style.opacity = 1;
+    setTimeout(() => {
+      // Display UI and welcome
+      labelWelcome.textContent = `Welcome back, ${currentAccount.owner
+        .split(" ")
+        .at(0)}`;
+      containerApp.style.opacity = 1;
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 3000);
   } else {
-    // Hide UI and warning sms
-    labelWelcome.textContent = "Login failed!";
-    containerApp.style.opacity = 0;
+    setTimeout(() => {
+      // Hide UI and warning sms
+      labelWelcome.textContent = "Login failed!";
+      containerApp.style.opacity = 0;
+    }, 3000);
   }
 
   // Clear fields
@@ -149,13 +163,19 @@ function displayMovements(account, sort = false) {
   moves.forEach((move, i) => {
     const type = move > 0 ? "deposit" : "withdrawal";
 
+    const formattedMove = formatCurrency(
+      move,
+      account.locale,
+      account.currency
+    );
+
     const html = `
         <div class="movements-row">
           <div class="movements-type movements-type-${type}">${
       i + 1
     } ${type}</div>
           <div class="movements-date">5 days ago</div>
-          <div class="movements-value">${move}$</div>
+          <div class="movements-value">${formattedMove}</div>
         </div>
     `;
 
@@ -173,13 +193,21 @@ function displaySummary(account) {
     .filter((move) => move > 0)
     .reduce((acc, deposit) => acc + deposit, 0);
 
-  labelSumIn.textContent = `${incomes}$`;
+  labelSumIn.textContent = formatCurrency(
+    incomes,
+    account.locale,
+    account.currency
+  );
   // Outcomes
   const outcomes = account.movements
     .filter((move) => move < 0)
     .reduce((acc, withdrawal) => acc + withdrawal, 0);
 
-  labelSumOut.textContent = `${Math.abs(outcomes)}$`;
+  labelSumOut.textContent = formatCurrency(
+    Math.abs(outcomes),
+    account.locale,
+    account.currency
+  );
   // Interest
   const interest = account.movements
     .filter((move) => move > 0)
@@ -187,7 +215,11 @@ function displaySummary(account) {
     .filter((interest) => interest >= 1)
     .reduce((acc, interest) => acc + interest, 0);
 
-  labelSumInterest.textContent = `${interest}$`;
+  labelSumInterest.textContent = formatCurrency(
+    interest,
+    account.locale,
+    account.currency
+  );
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -197,7 +229,11 @@ function displaySummary(account) {
 function displayBalance(account) {
   account.balance = account.movements.reduce((acc, move) => acc + move, 0);
 
-  labelBalance.textContent = `${account.balance}$`;
+  labelBalance.textContent = formatCurrency(
+    account.balance,
+    account.locale,
+    account.currency
+  );
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -223,15 +259,19 @@ btnTransfer.addEventListener("click", function (e) {
     currentAccount.username !== receiverAccount.username &&
     receiverAccount
   ) {
-    // Transfer money
-    currentAccount.movements.push(-amount);
-    receiverAccount.movements.push(amount);
-    // Update UI
-    updateUI(currentAccount);
-    // Show message
-    labelWelcome.textContent = "Transaction successful!";
+    setTimeout(() => {
+      // Transfer money
+      currentAccount.movements.push(-amount);
+      receiverAccount.movements.push(amount);
+      // Update UI
+      updateUI(currentAccount);
+      // Show message
+      labelWelcome.textContent = "Transaction successful!";
+    }, 3000);
   } else {
-    labelWelcome.textContent = "Transaction failed!";
+    setTimeout(() => {
+      labelWelcome.textContent = "Transaction failed!";
+    }, 3000);
   }
 });
 
@@ -248,14 +288,18 @@ btnLoan.addEventListener("click", function (e) {
     amount > 0 &&
     currentAccount.movements.some((move) => move >= amount * 0.1)
   ) {
-    // add positive movement into current account
-    currentAccount.movements.push(amount);
-    //update ui
-    updateUI(currentAccount);
-    // message
-    labelWelcome.textContent = "loan successful";
+    setTimeout(() => {
+      // add positive movement into current account
+      currentAccount.movements.push(amount);
+      //update ui
+      updateUI(currentAccount);
+      // message
+      labelWelcome.textContent = "loan successful";
+    }, 3000);
   } else {
-    labelWelcome.textContent = "loan not successful";
+    setTimeout(() => {
+      labelWelcome.textContent = "loan not successful";
+    }, 3000);
   }
 
   // clear
@@ -278,16 +322,20 @@ btnClose.addEventListener("click", function (e) {
       (account) => account.username === currentAccount.username
     );
 
-    // delete
-    accounts.splice(index, 1);
+    setTimeout(() => {
+      // delete
+      accounts.splice(index, 1);
 
-    // hide ui
-    containerApp.style.opacity = 0;
+      // hide ui
+      containerApp.style.opacity = 0;
 
-    // sms
-    labelWelcome.textContent = "account deleted";
+      // sms
+      labelWelcome.textContent = "account deleted";
+    }, 3000);
   } else {
-    labelWelcome.textContent = "delete can not be done";
+    setTimeout(() => {
+      labelWelcome.textContent = "delete can not be done";
+    }, 3000);
   }
 
   // clear fileds
